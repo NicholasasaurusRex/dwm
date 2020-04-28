925199271e24df71ddd5b87aa25b63dd7b091091
NicholasasaurusRex <nicholasasaurusrex@gmail.com>
2020-04-28 03:34:54 -0400

    Changes with colors

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 6;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  = 6;         /* border pixel of windows */
+static const unsigned int snap      = 32;        /* snap pixel */
+static const unsigned int gappih    = 0;         /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;         /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;         /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;         /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;         /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;         /* 0 means no bar */
+static const int topbar             = 1;         /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char dmenufont[]       = "monospace:size=10";
-static char normbgcolor[]           = "#000000";        /**/
-static char normbordercolor[]       = "#D7AFD7";	      /*#87AFD7*/
-static char normfgcolor[]           = "#C5C5C5";        /**/
-static char selfgcolor[]            = "#eeeeee";       /**/
-static char selbordercolor[]        = "#AFD787";        /**/
-static char selbgcolor[]            = "#87AFD7";	      /**/
+static char dmenufont[]             = "monospace:size=10";
+static char normbgcolor[]           = "#000000";          /**/
+static char normbordercolor[]       = "#D7AFD7";          /*#87AFD7*/
+static char normfgcolor[]           = "#C5C5C5";          /**/
+static char selfgcolor[]            = "#eeeeee";          /**/
+static char selbordercolor[]        = "#AFD787";          /**/
+static char selbgcolor[]            = "#87AFD7";          /**/
 static char *colors[][3] = {
-       /*               fg           bg           border   */
+       /*                  fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };

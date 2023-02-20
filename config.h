f440a897fa8acb4a25f28c91c447cba061c63737
Nick Jackson <mail@nickjackson.xyz>
2023-02-20 22:33:03 +0100

    update dwm

--- config.h
+++ config.h
@@ -20,8 +20,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int smartgaps          = 1;         /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;         /* 0 means no bar */
 static const int topbar             = 1;         /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10", "JoyPixels:pixelsize=12:antialias=true:autohint=true", \
-  "Source Code Pro:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Source Code Pro:size=14", "JoyPixels:pixelsize=14:antialias=true:autohint=true", \
+  "Source Code Pro:pixelsize=14:antialias=true:autohint=true" };
 /* static char dmenufont[]             = "monospace:size=10"; */
 static char dmenufont[]             = "Source Code Pro:size=10";
 static char normbgcolor[]           = "#000000";          /*#000000*/
